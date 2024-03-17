603883c97a51130d18ed4c765f2232a7074875b8
Maria Roman <mariarosariolevy_agbayani@yahoo.com>
2024-03-17 05:39:36 -0400

    added comments

--- automation-framework/src/main/java/stepDefinitions/Webstaurantstore.java
+++ automation-framework/src/main/java/stepDefinitions/Webstaurantstore.java
@@ -14,18 +14,19 @@ public class Webstaurantstore {
 
     public static void main(String[] args) throws InterruptedException
     {
+        // adding this here as I am getting 403 async error
         System.setProperty("webdriver.http.factory", "jdk-http-client");
-        // Mention here location of downloaded chrome driver path
+        // Mention here location of downloaded chrome driver path - tested this using Mac
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
                 + "/src/main/java/drivers/chromedriver");
 
+        // setting up chrome and getting url
         WebDriver driver = setupDriver();
-        driver.findElement(By.cssSelector("[id=\"searchval\"]")).sendKeys("stainless work table");
 
+        // searching for stainless work table
+        driver.findElement(By.cssSelector("[id=\"searchval\"]")).sendKeys("stainless work table");
         driver.findElement(By.xpath
                 ("//button[@value='Search']")).click();
-        Thread.sleep(5000);
-
 
         String table = "Table";
         List<WebElement> allSearchResults = driver.findElements(By.xpath("//span[@data-testid='itemDescription']"));
@@ -36,6 +37,7 @@ public class Webstaurantstore {
             Assert.assertTrue(eachResult.getText().contains(table), "Table not found!");
 
         }
+        Thread.sleep(5000);
         //assumed getting only the first page which has 60 items
         int iMaxSize = allSearchResults.size();
         WebElement lastTableElement = allSearchResults.getLast();
@@ -46,35 +48,39 @@ public class Webstaurantstore {
                 ("//div["+iMaxSize+"]/div/form/div/div/input[2][@data-testid='itemAddCart']")).click();
 
         // view cart
-        WebElement emptyCartJS = driver.findElement(By.xpath("//div[12]/div/p/div[2]/div[2]/a[1]")); // view cart button
+        WebElement emptyCartJS = driver.findElement(By.xpath("//div[12]/div/p/div[2]/div[2]/a[1]"));
 
         if(emptyCartJS != null){
-            emptyCartJS.click(); // click view cart from the JS window
-            Thread.sleep(5000);
+            // click view cart from the JS window
+            emptyCartJS.click();
+            Thread.sleep(5000); // leave this here to allow the cart to load
             // before emptying --  verify the last item was successfully added
             WebElement verifyLastItemAdded = driver.findElement(By.xpath("//div[2]/div/div[2]/div[1]/div[1]/div/div[2]/ul/li[2]/div/div[2]/span/a"));
             verifyLastItemAdded.getText().contains(lastItemAdded);
 
             // verify empty cart button is present - means there is a product in the cart
             WebElement emptyCart = driver.findElement(By.xpath("//div[1]/div[1]/div/div[1]/div/button"));
-            Thread.sleep(10000); // leave this here to allow the cart to load
+            //Thread.sleep(10000); // leave this here to allow the cart to load
             Assert.assertNotNull(emptyCart);
         }
         else {
-            // click view cart from the web icon
+            // click view cart from the web icon, if JS view cart is not present anymore
             driver.findElement(By.xpath("//div/div[1]/div[4]/a/span[1]")).click();
+            Thread.sleep(5000); // leave this here to allow the cart to load
             WebElement emptyCart = driver.findElement(By.xpath("//div[1]/div[1]/div/div[1]/div/button"));
-            Thread.sleep(10000); // leave this here to allow the cart to load
             Assert.assertNotNull(emptyCart);
         }
 
         // empty cart
-        driver.findElement(By.xpath("//div[1]/div[1]/div/div[1]/div/button")).click();
-        WebElement emptyCartConfirm =   driver.findElement(By.xpath("//div[11]/div/div/div/footer/button[1]"));
+        WebElement emptyCartConfirm = driver.findElement(By.xpath("//div[1]/div[1]/div/div[1]/div/button")); // empty cart button from view cart
+
 
         if(emptyCartConfirm != null){
+            driver.findElement(By.xpath("//div[1]/div[1]/div/div[1]/div/button")).click();
+            // click Empty Cart Confirmation window
             driver.findElement(By.xpath("//div[11]/div/div/div/footer/button[1]")).click();
-            Thread.sleep(10000); // leave this here to allow the cart to empty
+            Thread.sleep(5000); // leave this here to allow the cart to empty, or can wait for Cart to be present
+            // verify the cart was successfully empty
             WebElement emptyCartConfirmation = driver.findElement(By.xpath("//div[2]/div/div[2]/div[1]/div/div[1]/div[1]/div/div[2]/p[1]"));
             Assert.assertNotNull(emptyCartConfirmation, "Your cart is empty.");
         }
